eb41154b097beb95c888c3197713d5725554d086
Jeremiah Tabb <55849254+jtabb1@users.noreply.github.com>
2022-10-11 16:25:17 +0000

    implemented solution with little help

--- Main.java
+++ Main.java
@@ -12,19 +12,18 @@ class Main {
   }
 
     public int[] intersect(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        int i = 0, j = 0, k = 0;
-        while (i < nums1.length && j < nums2.length) {
-            if (nums1[i] < nums2[j]) {
-                ++i;
-            } else if (nums1[i] > nums2[j]) {
-                ++j;
-            } else {
-                nums1[k++] = nums1[i++];
-                ++j;
-            }
+      Arrays.sort(nums1);
+      Arrays.sort(nums2);
+      int i = 0, j = 0, k = 0;
+      while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] < nums2[j]) {
+          ++i;
+        } else if (nums1[i] > nums2[j]) {
+          ++j;
+        } else {
+          nums1[k++] = nums1[i++];
         }
-        return Arrays.copyOfRange(nums1, 0, k);
+      }
+      return Arrays.copyOfRange(nums1,0,k);
     }
 }
