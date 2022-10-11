01b06e21be6b77643d3d8c505ed6238507801a40
Jeremiah Tabb <55849254+jtabb1@users.noreply.github.com>
2022-10-11 15:45:30 +0000

    implement solution

--- Main.java
+++ Main.java
@@ -1,7 +1,30 @@
+import java.util.Arrays;
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    int[] in1 = new int[]{1,2,2,1};
+    int[] in2 = new int[]{2,2};
+    int[] inOut;
+    Main obj = new Main();
+    inOut = obj.intersect(in1, in2);
+    System.out.println(Arrays.toString(inOut));
   }
 
-  
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0, j = 0, k = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                ++i;
+            } else if (nums1[i] > nums2[j]) {
+                ++j;
+            } else {
+                nums1[k++] = nums1[i++];
+                ++j;
+            }
+        }
+        return Arrays.copyOfRange(nums1, 0, k);
+    }
 }
